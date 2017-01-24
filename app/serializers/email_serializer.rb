56c39ab60e5438a566689920d73be55ed8b25255
rasg <rodolfo@mutalis.com>
2017-01-24 11:59:42 -0600

    Removes the edit_it attribute from the email serializer.

--- app/serializers/email_serializer.rb
+++ app/serializers/email_serializer.rb
@@ -1,10 +1,6 @@
 # EmailSerializer
 class EmailSerializer < ApplicationSerializer
-  attributes :id, :username, :quota, :domain_id, :edit_it
-
-  def edit_it
-    v2_email_url(object)
-  end
+  attributes :id, :username, :quota, :domain_id
 
   # def default_url_options
   #   { host: 'api.example.com' }
