75eda81b366d353262fa696552d1e70511ab79ef
rasg <rodolfo@mutalis.com>
2017-01-25 19:41:11 -0600

    Appends the domain name to the username in the email Factory definition

--- test/factories/emails.rb
+++ test/factories/emails.rb
@@ -1,6 +1,6 @@
 FactoryGirl.define do
   factory :email do
-    username { Faker::Internet.user_name }
+    username { Faker::Internet.user_name + '@' + domain.name.to_s }
     quota { (rand * 100 + 1).to_i }
     password { Faker::Internet.password(8) }
     domain
