73821d2bb28a1a6cdbbf41424c568ed5cfe02fe5
rasg <rodolfo@mutalis.com>
2017-01-26 19:31:56 -0600

    Adds files we want symlinking to specific entries in shared directory

--- config/deploy.rb
+++ config/deploy.rb
@@ -26,7 +26,7 @@ set :deploy_to, "/home/#{fetch(:user)}/#{fetch(:application)}"
 set :pty, true
 
 # Default value for :linked_files is []
-# set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
+set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
 
 # append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system'
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system', 'public/assets', 'vendor/bundle', 'public/spree')
