b3139410fd3f46627b4f77d01b30fb2769a1b7cd
rasg <rodolfo@mutalis.com>
2017-01-28 00:29:16 -0600

    Removes the constraint to only use the subdomain name "api" in the hostname

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,10 @@
 Rails.application.routes.draw do
-  constraints subdomain: 'api' do
-    namespace :v1 do
-      resources :emails
-    end
-    namespace :v2 do
-      resources :domains do
-        resources :emails, shallow: true
-      end
+  namespace :v1 do
+    resources :emails
+  end
+  namespace :v2 do
+    resources :domains do
+      resources :emails, shallow: true
     end
   end
 end
